4717f38b03a9163e80e5b48d25bcaaa2ef78c9e8
King's meow <mingee641@gmail.com>
2023-11-15 17:22:42 +0900

    Feat: 날짜 정보를 담은 DTO 클래스 추가

---
+++ src/main/java/christmas/dto/DateDTO.java
@@ -0,0 +1,15 @@
+package christmas.dto;
+
+public class DateDTO {
+
+    private int date;
+
+    public DateDTO(int date) {
+        this.date = date;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+}
