0cc99da36e56587df58cb2bde43e73da33a92353
King's meow <mingee641@gmail.com>
2023-11-15 20:29:57 +0900

    Refactor: 필드변수 추가

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -5,10 +5,12 @@ public class Order {
     private Long id;
     private final Date date;
     private final OrderMenu orderMenu;
+    private final int totalPrice;
 
     public Order(Date date, OrderMenu orderMenu) {
         this.date = date;
         this.orderMenu = orderMenu;
+        this.totalPrice = calculateTotalPrice();
     }
 
     public Date getDate() {
@@ -19,11 +21,19 @@ public class Order {
         return orderMenu;
     }
 
+    public int getTotalPrice() { return totalPrice; }
+
     public void setId(Long id) {
         this.id = id;
     }
 
     public Long getId() {
         return id;
+    }
+
+    public int calculateTotalPrice() {
+        return orderMenu.getMenus().entrySet().stream()
+                .mapToInt(entry -> entry.getKey().getMenuPrice() * entry.getValue())
+                .sum();
     }
 }
