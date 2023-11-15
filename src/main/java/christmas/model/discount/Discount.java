6b931a4c1225519233a91e13ef5ab666ce3f3920
King's meow <mingee641@gmail.com>
2023-11-15 19:13:57 +0900

    Refactor: 추상 클래스로 변경 및 형식 개선

--- src/main/java/christmas/model/discount/Discount.java
+++ src/main/java/christmas/model/discount/Discount.java
@@ -1,11 +1,20 @@
 package christmas.model.discount;
 
 
+import java.text.NumberFormat;
 
-public interface Discount {
+public abstract class Discount {
+    protected int discountPrice;
+    protected String discountName;
 
-    void calcuateDiscount(int date, int count);
-    String toString();
-    int getDiscountPrice();
+    abstract public void calcuateDiscount(int date, int count);
+    public String toString() {
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        String formattedDiscountPrice = numberFormat.format(discountPrice);
+        return discountName + ": -" + formattedDiscountPrice + "원";
+    };
+    public int getDiscountPrice() {
+        return discountPrice;
+    };
 
 }
