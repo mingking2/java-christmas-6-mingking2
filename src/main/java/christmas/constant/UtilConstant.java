87b11c380c6d0dc84befb8dea5c2feee14acb2be
King's meow <mingee641@gmail.com>
2023-11-15 17:36:56 +0900

    Feat: 형변환을 위한 상수들 UtilConstant 클래스 추가

---
+++ src/main/java/christmas/constant/UtilConstant.java
@@ -0,0 +1,10 @@
+package christmas.constant;
+
+public class UtilConstant {
+
+
+    public static final String SPLIT_STREAM_REGEX = ",";
+    public static final String SPLIT_STREAM_MAP_REGEX = "-";
+    public static final String NUMERIC_REGEX = "-?\\d+(\\.\\d+)?";
+
+}
