f7dda083640f0d73a45eb0f238c5c5ffe0dfca15
King's meow <mingee641@gmail.com>
2023-11-15 17:32:05 +0900

    Feat: 주문 정보 저장 및 조회 기능을 위한 OrderRepository 클래스 추가

---
+++ src/main/java/christmas/repository/OrderRepository.java
@@ -0,0 +1,29 @@
+package christmas.repository;
+
+import christmas.model.Order;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class OrderRepository {
+
+    private static final Map<Long, Order> store = new ConcurrentHashMap<>();
+    private static long sequence = 0L;
+
+    private static final OrderRepository instance = new OrderRepository();
+
+    public static OrderRepository getInstance() {
+        return instance;
+    }
+
+
+    public Order save(Order order) {
+        order.setId(++sequence);
+        store.put(order.getId(), order);
+        return order;
+    }
+
+    public Order findById(Long id) {
+        return store.get(id);
+    }
+
+}
